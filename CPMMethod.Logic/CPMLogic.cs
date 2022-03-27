fcec7cd6dfe49b646dfa2cd684222cee94ca6897
Dawid Blachowicz <david.blachowicz@gmail.com>
2022-03-27 23:43:35 +0200

    Added Activities sorting

--- CPMMethod.Logic/CPMLogic.cs
+++ CPMMethod.Logic/CPMLogic.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace CPMMethod.Logic
 {
     public class CPMLogic
@@ -6,13 +8,15 @@ namespace CPMMethod.Logic
         {
             return 0;
         }
+
         private static uint GetMaxLateStart(Activity[] activities)
         {
             return 0;
         }
+
         private static Activity[] CalculateEarly(Activity[] activities)
         {
-            foreach(Activity activity in activities)
+            foreach (Activity activity in activities)
             {
                 activity.EarlyStart = GetMinEarlyFinish(activity.Preccessors);
                 activity.EarlyFinish = activity.EarlyStart + activity.Duration;
@@ -20,9 +24,10 @@ namespace CPMMethod.Logic
 
             return activities;
         }
+
         private static Activity[] CalculateLate(Activity[] activities)
         {
-            foreach(Activity activity in activities)
+            foreach (Activity activity in activities)
             {
                 activity.LateFinish = GetMaxLateStart(activity.Successors);
                 activity.LateStart = activity.LateFinish - activity.Duration;
@@ -30,9 +35,75 @@ namespace CPMMethod.Logic
 
             return activities;
         }
+
         private static String GetCriticalPath(Activity[] acitvities)
         {
             return String.Empty;
+        }
+
+        private static Activity[] Sort(Activity[] activities, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = l + (r - 1) - 2;
+
+                Sort(activities, l, m);
+                Sort(activities, m + 1, r);
+                Merge(activities, l, m, r);
+            }
+            
+            return activities;
+        }
+
+        private static void Merge(Activity[] activities, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            Activity[] L = new Activity[n1];
+            Activity[] R = new Activity[n2];
+
+            int i, j;
+            for (i = 0; i < n1; i++)
+                L[i] = activities[l + i];
+            for (j = 0; j < n2; j++)
+                R[j] = activities[m + 1 + j];
+
+            int k = 1;
+            i = 0;
+            j = 0;
+            while (i < n1 && j < n2)
+            {
+                foreach (Activity act in R[j].Preccessors)
+                {
+                    if (Convert.ToInt16(L[i].Id) <= Convert.ToInt16(act.Id))
+                    {
+                        activities[k] = L[i];
+                        i++;
+                    }
+                    else
+                    {
+                        activities[k] = R[j];
+                        j++;
+                    }
+
+                    k++;
+                }
+            }
+
+            while (i < n1)
+            {
+                activities[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                activities[k] = R[j];
+                j++;
+                k++;
+            }
         }
     }
 }
