2801ea72f4dbd0c93124dc2ac4af9b5a81d6f939
patrykowski2 <patrykowski098@wp.pl>
2022-03-27 17:11:01 +0200

    Added activity class implemetiation XD

--- CPMMethod.Logic/Activity.cs
+++ CPMMethod.Logic/Activity.cs
@@ -2,6 +2,10 @@ namespace CPMMethod.Logic
 {
     public class Activity
     {
-        
+        public string Id { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public uint Duration { get; set; }
+        public Activity[] Successors { get; set; } 
+        public Activity[] Preccessors { get; set; }
     }
 }
