451dc38b0b0a79b6e8c6a450c889af3b0dd87b76
PiotrApriasz <PPlacki500>
2022-03-28 00:32:07 +0200

    Prepare Data Grid kurwa mać jakie to było jebane gówno

--- CPMMethod.Logic/Activity.cs
+++ CPMMethod.Logic/Activity.cs
@@ -1,10 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using CPMMethod.BlazorWasmClient.Validators;
+
 namespace CPMMethod.Logic
 {
     public class Activity
     {
+        [Display(Name = "Id Aktywności")]
+        [Required(ErrorMessage = "Id Aktywności jest wymagane")]
+        [CustomValidationActivityId]
         public string Id { get; set; } = String.Empty;
+        
+        [Display(Name = "Opis")]
         public string Description { get; set; } = String.Empty;
+        
+        [Display(Name = "Czas Trwania")]
         public uint Duration { get; set; }
+        
+        [Display(Name = "Aktywność Poprzedzająca")]
+        [Required(ErrorMessage = "Aktywność poprzedzająca jest wymagana")]
+        public string PreActivities { get; set; }
+        
         public Activity[] Successors { get; set; } 
         public Activity[] Preccessors { get; set; }
     }
