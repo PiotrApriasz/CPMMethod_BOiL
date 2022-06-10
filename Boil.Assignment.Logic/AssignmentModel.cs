3583ad622c734c900b6f0cc86e906ea3c1d24c9d
patrykowski2 <patrykowski098@wp.pl>
2022-06-10 14:37:49 +0200

    Added logic implementation

--- Boil.Assignment.Logic/AssignmentModel.cs
+++ Boil.Assignment.Logic/AssignmentModel.cs
@@ -2,6 +2,7 @@
 
 public class AssignmentModel
 {
+    // Init data
     public int[,] TransportCost { get; set; }
     public int[] Demand { get; set; }
     public int[] Supply { get; set; }
@@ -10,6 +11,10 @@ public class AssignmentModel
     public int SupplierCount { get; set; }
     public int RecipientCount { get; set; }
     public int[,] UnitProfit { get; set; }
+    // Calculation data
+    public int?[] Alpha { get; set; }
+    public int?[] Beta { get; set; }
+    // CHUJ WIE CO TO ZA DATA
     public int MaximumMiddlemanProfit { get; set; }
     
     //Tabela na optymalne plany przewozów dla każdej iteracji i dla ustawienia początkowego. Finalna tabela zostanie wypisana.
@@ -29,7 +34,8 @@ public class AssignmentModel
         SellingCost = new int[recipientCount];
         UnitProfit = new int[SupplierCount, RecipientCount];
         OptimalTransportPlan = new int[SupplierCount, RecipientCount];
-        
+        Alpha = new int?[RecipientCount];
+        Beta = new int?[SupplierCount];
 
         for (int i = 0; i < supplierCount; i++)
         {
@@ -80,5 +86,74 @@ public class AssignmentModel
 
         Supply[SupplierCount - 1] = fullDemand;
         Demand[RecipientCount - 1] = fullSupply;
+    }
+
+    public (int, int) FindMaxElementPos(int?[,] matrix) {
+
+        var max_value = matrix.Cast<int?>().Max();
+        
+        for(int xi = 0; xi < SupplierCount; xi++) {
+            for(int yi = 0; yi < RecipientCount; yi++) {
+
+                if(matrix[xi, yi].Equals(max_value)) {
+
+                    return (xi, yi);
+                }
+            }
+        }
+
+        return (-1, -1);
+    }
+    public void NorthWestCornerMethod() {
+        for(int xi = 0; xi < SupplierCount; xi++) {
+            for(int yi = 0; yi < RecipientCount; yi++) {
+                int quantity = Math.Min(Demand[yi], Supply[xi]);
+                if(quantity > 0) {
+                    OptimalTransportPlan[xi, yi] = quantity;
+                    Demand[yi] -= quantity;
+                    Supply[xi] -= quantity;
+                }
+            }
+        }
+    }
+
+    public void CalculateAlphaBeta() {
+        // NIEWIEM KURWA JAK TO NAPISAC
+    }
+
+    public ((int, int), (int, int), (int, int), (int, int)) FindCycle() {
+        int?[,] cycleMatrix = new int?[SupplierCount, RecipientCount];
+        for(int xi = 0; xi < SupplierCount; xi++) {
+            for(int yi = 0; yi < RecipientCount; yi++) {
+                if(OptimalTransportPlan[xi ,yi] == 0) {
+                    cycleMatrix[xi, yi] = UnitProfit[xi, yi] - Alpha[yi].GetValueOrDefault() - Beta[xi].GetValueOrDefault();
+                }
+            }
+        }
+        var (x, y) = FindMaxElementPos(cycleMatrix);
+        if(cycleMatrix[x, y] < 0) {
+            return ((-1, -1), (-1, -1), (-1, -1), (-1, -1));
+        }
+        for(int xi = 0; xi < SupplierCount; xi++) {
+            for(int yi = 0; yi < RecipientCount; yi++) {
+                if(x == xi || y == yi || cycleMatrix[xi, yi] != null) {
+                    break;
+                }
+                if(cycleMatrix[x, yi] == null && cycleMatrix[xi, y] == null) {
+                    return ((x, y), (xi, yi), (x, yi), (xi, y));
+                }
+            }
+        }
+        return ((-1, -1), (-1, -1), (-1, -1), (-1, -1));
+    }
+
+    public void DoCycle() {
+        var ((x1, y1), (x2, y2), (x3, y3), (x4, y4)) = FindCycle();
+        if (x1 == -1) { return; }
+        int MinValue = (new int[] {OptimalTransportPlan[x1, y1], OptimalTransportPlan[x2, y2], OptimalTransportPlan[x3, y3], OptimalTransportPlan[x4, y4]}).Min();
+        OptimalTransportPlan[x1, y1] += MinValue;
+        OptimalTransportPlan[x2, y2] += MinValue;
+        OptimalTransportPlan[x3, y3] -= MinValue;
+        OptimalTransportPlan[x4, y4] -= MinValue;
     }
 }
